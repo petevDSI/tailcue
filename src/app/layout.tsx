79c80774877af58f0d10f14ba66a756e806995b1
Peter Volk <petegvolk@gmail.com>
2026-06-24 23:07:48 -0400

    feat: add Impact.com Petco tracking tag to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import BottomTabBar from '@/components/BottomTabBar'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -20,6 +21,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <Script
+          id="impact-stat"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7435639-31b3-4f2b-a4eb-8c10ebfb87611.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`
+          }}
+        />
+      </head>
       <body className="font-sans antialiased" style={{ backgroundColor: '#FFFBF0' }}>
         {children}
         <BottomTabBar />
